90c788095d4df33e3dcb2660d910e8973e45bb1d
nehaprabhavalkar <nehaprabhavalkar47@gmail.com>
2022-01-19 12:30:42 +0530

    added data_cleaning module

---
+++ code/data_cleaning.py
@@ -0,0 +1,48 @@
+import re
+import pandas as pd
+from datetime import datetime
+import warnings
+warnings.filterwarnings("ignore")
+from scraping import save_to_csv
+
+DATA_PATH = '../data/'
+
+def clean_dataset(df):
+    for i in range(0,len(df)):
+        df['title'][i] = re.sub('[^A-Za-z0-9]+', ' ', df['title'][i])
+        df['title'][i] = df['title'][i].strip()
+        
+    for i in range(0,len(df)):
+        df['description'][i] = df['description'][i].strip()
+        
+    pattern = r'\[.*?\]'
+    for i in range(0,len(df)):
+        df['views'][i] = re.sub(pattern, '', df['views'][i])
+
+    pattern = r'\(.*?\)'
+    for i in range(0,len(df)):
+        df['dates'][i] = re.sub(pattern, '', df['dates'][i])
+        df['dates'][i] = df['dates'][i].replace('\xa0',' ').strip()
+        
+    for i in range(0,len(df)):
+        df['dates'][i] = datetime.strptime(df['dates'][i], '%B %d, %Y')
+        df['dates'][i]=df['dates'][i].date()
+        
+    df['views'] = pd.to_numeric(df['views'])
+    
+    df['title'] = df['title'].astype(str)
+    
+    print("Dataset cleaned successfully")
+    
+    return df
+
+
+if __name__ == '__main__':
+
+    file_name = 'office.csv'
+
+    df = pd.read_csv(DATA_PATH + file_name)
+
+    cleaned_df = clean_dataset(df)
+
+    save_to_csv(cleaned_df, file_name)
--- code/scraping.py
+++ code/scraping.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup 
 import pandas as pd
-import re
 import warnings
 warnings.filterwarnings("ignore")
 from datetime import datetime
