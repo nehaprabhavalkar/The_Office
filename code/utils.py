e91eef0c90803cbdd535a2a42d392c8dbe1fcd26
nehaprabhavalkar <nehaprabhavalkar47@gmail.com>
2022-01-26 19:44:28 +0530

    added graphs

--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,22 @@
 import yaml
+import plotly.graph_objects as go
 
 def load_yaml_data():
     with open('../conf/config.yml', 'r') as file:
         data = yaml.safe_load(file)
+        
     return data
+
+
+def plot_indicator_chart(value, title_str):
+
+    fig = go.Figure(go.Indicator(
+        mode = 'number',
+        value = value,
+        title = {'text': title_str}
+    
+    ))
+
+    fig.update_layout(template='plotly_dark')
+
+    return fig
